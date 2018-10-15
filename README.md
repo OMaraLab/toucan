65e6c9b036b9d402f4a6ef91f26ef0404c43fdfd
Lily Wang <lwang@live.com.au>
2018-10-16 02:28:15 +1100

    Initial commit after CMS Cookiecutter creation

---
+++ README.md
@@ -0,0 +1,18 @@
+toucan
+==============================
+[//]: # (Badges)
+[![Travis Build Status](https://travis-ci.org/REPLACE_WITH_OWNER_ACCOUNT/toucan.png)](https://travis-ci.org/REPLACE_WITH_OWNER_ACCOUNT/toucan)
+[![AppVeyor Build status](https://ci.appveyor.com/api/projects/status/REPLACE_WITH_APPVEYOR_LINK/branch/master?svg=true)](https://ci.appveyor.com/project/REPLACE_WITH_OWNER_ACCOUNT/toucan/branch/master)
+[![codecov](https://codecov.io/gh/REPLACE_WITH_OWNER_ACCOUNT/toucan/branch/master/graph/badge.svg)](https://codecov.io/gh/REPLACE_WITH_OWNER_ACCOUNT/toucan/branch/master)
+
+Tools and utilities for the can-do user
+
+### Copyright
+
+Copyright (c) 2018, OMaraLab
+
+
+#### Acknowledgements
+ 
+Project based on the 
+[Computational Molecular Science Python Cookiecutter](https://github.com/molssi/cookiecutter-cms)
---
+++ setup.py
@@ -0,0 +1,42 @@
+"""
+toucan
+Tools and utilities for the can-do user
+"""
+from setuptools import setup
+import versioneer
+
+DOCLINES = __doc__.split("\n")
+
+setup(
+    # Self-descriptive entries which should always be present
+    name='toucan',
+    author='OMaraLab',
+    description=DOCLINES[0],
+    long_description="\n".join(DOCLINES[2:]),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license='LGPLv3',
+
+    # Which Python importable modules should be included when your package is installed
+    packages=['toucan', "toucan.tests"],
+
+    # Optional include package data to ship with your package
+    # Comment out this line to prevent the files from being packaged with your software
+    # Extend/modify the list to include/exclude other items as need be
+    package_data={'toucan': ["data/*.dat"]
+                  },
+
+    # Additional entries you may want simply uncomment the lines you want and fill in the data
+    # author_email='me@place.org',      # Author email
+    # url='http://www.my_package.com',  # Website
+    # install_requires=[],              # Required packages, pulls from pip if needed; do not use for Conda deployment
+    # platforms=['Linux',
+    #            'Mac OS-X',
+    #            'Unix',
+    #            'Windows'],            # Valid platforms your code works on, adjust to your flavor
+    # python_requires=">=3.5",          # Python version restrictions
+
+    # Manual control if final package is compressible or not, set False to prevent the .egg from being made
+    # zip_safe=False,
+
+)
